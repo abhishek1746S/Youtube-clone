38d841d954b83bf5befe09efffa50789a1f2de87
Abhishek Kumar <ak9984115@gmail.com>
2026-05-06 21:24:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./header/Header";
+import Footer from "./Footer/Footer";
+import "./App.css"
+function App() {
+  return (
+   <>
+   <div className="parent flex flex-col">
+    <div><Header/></div>
+
+    <main className="flex-1 "></main>  {/* 🔥 main fix */}
+
+    <Footer/>
+
+   </div>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./Footer.module.css"
+const Footer = () => {
+  return (
+      <footer className="flex mt-auto">
+              <ul className={styles.ulfl}>
+              <li><img src="https://img.icons8.com/?size=45&id=86527&format=png&color=000000" alt="" /></li>
+              <li><img src="https://img.icons8.com/?size=45&id=ajczeHCWXbyR&format=png&color=000000" alt="" /></li>
+              <li><img src="https://img.icons8.com/?size=45&id=24717&format=png&color=000000" alt="" /></li>
+              <li><img src="https://img.icons8.com/?size=45&id=reqYJP3X0f7Y&format=png&color=000000" alt="" /></li>
+              <li><img src="https://img.icons8.com/?size=45&id=85050&format=png&color=000000" alt="" /></li>
+              </ul>
+      </footer>
+  )
+}
+
+export default Footer
